f85b640cd5d975984ae4bb8529dd4575285f7ac7
abezer <abezes.haile@gmail.com>
2018-09-18 17:06:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+   /* A program is needed to prompt the user operator for ten exam scores, calculate the average score and display
+   the result to the screen.
+    The average score is calculated as the sum of the scores divided by the number of scores
+*/
+
+
+    public static void main(String[] args) {
+
+        int count = 1;
+      /*  do {
+            if(count ==5){
+                System.out.println("woohoo");
+            }else
+
+            System.out.println(count);
+            count++;
+        }while (count<=10);*/
+
+        while (count <= 10) {
+
+            if (count == 5) {
+                System.out.println("woohoo");
+            } else {
+                System.out.println(count);
+            }count++;
+        }
+
+
+    }
+}
